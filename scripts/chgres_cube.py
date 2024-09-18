bee31be9fb0f8cc1ef0bd95bb9cf78b23ab11a93
WeirAE <weirbs@gmail.com>
2024-09-18 16:41:01 -0500

    try some changes in chgres_cube.py

--- scripts/chgres_cube.py
+++ scripts/chgres_cube.py
@@ -46,17 +46,15 @@ args = parser.parse_args()
 
 os.environ["member"] = args.member
 
-
-# fix CRES dereferencing
-expt_config = get_yaml_config(args.config_file) 
-os.environ["CRES"] = expt_config["workflow"]["CRES"]
-expt_config.dereference(
-	context={
-		**os.environ,
-		**expt_config_cp,
-	}
-)
+expt_config = get_yaml_config(args.config_file)
 chgres_cube_config = expt_config[args.key_path]
+
+rundir = Path(chgres_cube_config["rundir"])
+print(f"Will run in {rundir}")
+
+CRES = expt_config["workflow"]["CRES"]
+os.environ["CRES"] = CRES
+
 
 # Extract driver config from experiment config
 chgres_cube_driver = Chgres_Cube(
@@ -64,22 +62,23 @@ chgres_cube_driver = Chgres_Cube(
     cycle=args.cycle,
     key_path=[args.key_path],
 )
-rundir = Path(chgres_cube_driver.config["rundir"])
-print(f"Will run in {rundir}")
 
+# update fn_atm and fn_sfc for ics task
+if args.key_path == "task_make_ics":
+    varsfilepath = chgres_cube_driver.config["task_make_ics"][
+        "input_files_metadata_path"
+    ]
+    extrn_config_fns = get_sh_config(varsfilepath)["EXTRN_MDL_FNS"]
+    extrn_config_fhrs = get_sh_config(varsfilepath)["EXTRN_MDL_FHRS"]
 
-if args.key_path == "task_make_ics"
-varsfilepath = chgres_cube_driver.config["task_make_ics"]["input_files_metadata_path"]
-extrn_config_fns = get_sh_config(varsfilepath)["EXTRN_MDL_FNS"]
-extrn_config_fhrs = get_sh_config(varsfilepath)["EXTRN_MDL_FHRS"]
+    fn_atm = extrn_config_fns[0]
+    fn_sfc = extrn_config_fns[1]
 
-# make_ics
-fn_atm = extrn_config_fns[0]
-fn_sfc = extrn_config_fns[1]
+    chgres_cube_driver.run()
 
-# Loop the run of chgres_cube for the forecast length
-if len(extrn_config_fns) > 2:
-    fn_sfc= ""
+# Loop the run of chgres_cube for the forecast length if lbcs
+else:
+    fn_sfc = ""
     num_fhrs = chgres_cube_driver.config["workflow"]["FCST_LEN_HRS"]
     bcgrp10 = 0
     bcgrpnum10 = 1
@@ -88,17 +87,18 @@ if len(extrn_config_fns) > 2:
         if i < num_fhrs:
             print(f"group ${bcgrp10} processes member ${i}")
             fn_atm = f"${{EXTRN_MDL_FNS[${i}]}}"
-        
-            expt_config["task_make_lbcs"]["chgres_cube"]["namelist"]["update_values"]["config"]["atm_files_input_grid"] = fn_atm
+
+            expt_config["task_make_lbcs"]["chgres_cube"]["namelist"]["update_values"][
+                "config"
+            ]["atm_files_input_grid"] = fn_atm
             # reinstantiate driver
             chgres_cube_driver = Chgres_Cube(
-                    config=expt_config,
-                    cycle=args.cycle,
-                    key_path=[args.key_path],
-                )
+                config=expt_config,
+                cycle=args.cycle,
+                key_path=[args.key_path],
+            )
             chgres_cube_driver.run()
-else:
-    chgres_cube_driver.run()
+
 
 # error message
 if not (rundir / "runscript.chgres_cube.done").is_file():
@@ -127,14 +127,19 @@ for label in chgres_cube_config["output_file_labels"]:
     lbc_block = expt_config_cp[args.key_path]
     lbc_input_fn = "gfs.bndy.nc"
     lbc_spec_fhrs = extrn_config_fhrs[i]
-    lbc_offset_fhrs = chgres_cube_driver.config["task_get_extrn_lbcs"]["EXTRN_MDL_LBCS_OFFSET_HRS"]
+    lbc_offset_fhrs = chgres_cube_driver.config["task_get_extrn_lbcs"][
+        "EXTRN_MDL_LBCS_OFFSET_HRS"
+    ]
     nco_net = chgres_cube_driver.config["nco"]["NET_default"]
     dot_ensmem = f".mem{ args.member }"
-    fcst_hhh = ( lbc_spec_fhrs - lbc_offset_fhrs )
+    fcst_hhh = lbc_spec_fhrs - lbc_offset_fhrs
     fcst_hhh_FV3LAM = print(f"fcst_hhh:03d")
 
-    lbc_output_fn = rundir / f"{nco_net}.{args.cycle}{dot_ensmem}.gfs_bndy.tile7.f{fcst_hhh_FV3LAM}.nc"
-    
+    lbc_output_fn = (
+        rundir
+        / f"{nco_net}.{args.cycle}{dot_ensmem}.gfs_bndy.tile7.f{fcst_hhh_FV3LAM}.nc"
+    )
+
     links[lbc_input_fn] = str(lbc_output_fn)
 
 uwlink(target_dir=rundir.parent, config=links)
