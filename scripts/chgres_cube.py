fc592043bff23993751dfbce1b853d146844c0f8
WeirAE <weirbs@gmail.com>
2024-10-16 18:30:31 +0000

    Fix file copy logic

--- scripts/chgres_cube.py
+++ scripts/chgres_cube.py
@@ -154,7 +154,6 @@ def run_chgres_cube(config_file, cycle, key_path, member):
             }
         }
         expt_config_cp.update_from(update_cfg)
-        logging.info(f"updated config: {expt_config_cp}")
 
         # reinstantiate driver
         chgres_cube_driver = ChgresCube(
@@ -163,6 +162,31 @@ def run_chgres_cube(config_file, cycle, key_path, member):
             key_path=key_path,
         )
         chgres_cube_driver.run()
+
+        # Deliver output data to a common location above the rundir.
+        links = {}
+
+        nco_net = expt_config["nco"]["NET_default"]
+        dot_ensmem = f".mem{member}"
+        tile_rgnl = expt_config["constants"]["TILE_RGNL"]
+        nh0 = expt_config["constants"]["NH0"]
+        cyc = str(expt_config["workflow"]["DATE_FIRST_CYCL"])[-2:]
+
+        output_dir = os.path.join(os.path.dirname(rundir.parent), "INPUT")
+        links[f"out.atm.tile{tile_rgnl}.nc"] = str(
+            f"{output_dir}{nco_net}.t{cyc}z{dot_ensmem}.gfs_data.tile{tile_rgnl}.halo{nh0}.nc"
+        )
+        links[f"out.sfc.tile{tile_rgnl}.nc"] = str(
+            f"{output_dir}{nco_net}.t{cyc}z{dot_ensmem}.sfc_data.tile{tile_rgnl}.halo{nh0}.nc"
+        )
+        links[f"gfs_ctrl.nc"] = str(
+            f"{output_dir}{nco_net}.t{cyc}z{dot_ensmem}.gfs_ctrl.nc"
+        )
+        links[f"gfs.bndy.nc"] = str(
+            f"{output_dir}{nco_net}.t{cyc}z{dot_ensmem}.gfs_bndy.tile{tile_rgnl}.f000.nc"
+        )
+
+        uwlink(target_dir=rundir, config=links)
 
     # Loop the run of chgres_cube for the forecast length if lbcs
     else:
@@ -193,7 +217,7 @@ def run_chgres_cube(config_file, cycle, key_path, member):
                     update = {"atm_files_input_grid": fn_atm}
 
                 update_cfg = {
-                    "task_make_ics": {
+                    "task_make_lbcs": {
                         "chgres_cube": {
                             "namelist": {"update_values": {"config": update}}
                         }
@@ -209,45 +233,33 @@ def run_chgres_cube(config_file, cycle, key_path, member):
                 )
                 chgres_cube_driver.run()
 
+                # Deliver output data to a common location above the rundir.
+                links = {}
+
+                lbc_spec_fhrs = extrn_config_fhrs[i]
+                lbc_offset_fhrs = expt_config_cp["task_get_extrn_lbcs"][
+                    "EXTRN_MDL_LBCS_OFFSET_HRS"
+                ]
+                fcst_hhh = int(lbc_spec_fhrs) - int(lbc_offset_fhrs)
+                fcst_hhh_FV3LAM = f"{fcst_hhh:03d}"
+                cyc = str(expt_config["workflow"]["DATE_FIRST_CYCL"])[-2:]
+
+                nco_net = expt_config["nco"]["NET_default"]
+                dot_ensmem = f".mem{member}"
+
+                lbc_input_fn = "gfs.bndy.nc"
+                output_dir = os.path.join(os.path.dirname(rundir.parent), "INPUT")
+                lbc_output_fn = str(
+                    f"{output_dir}{nco_net}.t{cyc}z{dot_ensmem}"
+                    f".gfs_bndy.tile7.f{fcst_hhh_FV3LAM}.nc"
+                )
+                links[lbc_input_fn] = str(lbc_output_fn)
+                uwlink(target_dir=rundir, config=links)
+
     # error message
     if not (rundir / "runscript.chgres_cube.done").is_file():
         print("Error occurred running chgres_cube. Please see component error logs.")
         sys.exit(1)
-
-    # Deliver output data
-    expt_config = get_yaml_config(config_file)
-    chgres_cube_config = _walk_key_path(expt_config, key_path)
-
-    # Move initial condition, surface, control, and 0-th hour lateral bound-
-    # ary files to ICs_BCs directory.
-    links = {}
-    for label in chgres_cube_config["output_file_labels"]:
-        # deepcopy here because desired_output_name is parameterized within the loop
-        expt_config_cp = get_yaml_config(deepcopy(expt_config.data))
-        expt_config_cp.dereference(
-            context={
-                "cycle": cycle,
-                "file_label": label,
-                **expt_config_cp,
-            }
-        )
-        lbc_block = _walk_key_path(expt_config_cp, key_path)
-        lbc_input_fn = "gfs.bndy.nc"
-        lbc_spec_fhrs = extrn_config_fhrs[i]
-        lbc_offset_fhrs = lbc_block["EXTRN_MDL_LBCS_OFFSET_HRS"]
-        nco_net = expt_config["nco"]["NET_default"]
-        dot_ensmem = f".mem{member}"
-        fcst_hhh = lbc_spec_fhrs - lbc_offset_fhrs
-        fcst_hhh_FV3LAM = f"{fcst_hhh:03d}"
-
-        lbc_output_fn = (
-            rundir
-            / f"{nco_net}.{cycle}{dot_ensmem}.gfs_bndy.tile7.f{fcst_hhh_FV3LAM}.nc"
-        )
-
-        links[lbc_input_fn] = str(lbc_output_fn)
-
-    uwlink(target_dir=rundir.parent, config=links)
 
 
 if __name__ == "__main__":
